28185565f107ddb08ad2f39e60360693086df3af
Tejasri-4183 <karnamtejasri09@gmail.com>
2025-12-15 16:50:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".primary-btn").forEach(button => {
+  button.addEventListener("click", () => {
+    alert("Thank you for your interest. Our team will contact you shortly.");
+  });
+});
